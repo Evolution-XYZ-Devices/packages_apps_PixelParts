d2e00729f43969362fe1ef8d1628b6f415341eaa
Anierin Bliss <anierin@evolution-x.org>
2023-03-02 19:48:51 +0000

    PixelParts: Initial app import
    Current features:
    - Direct HBM toggle
    - AutoHBM toggle & threshold preference
    - Fsync toggle
    - Kcal Red, Green, Blue, Saturation, Contrast & Value preferences
    - Power efficient workqueue toggle
    - Sound control mic & speaker gain preferences
    - USB 2.0 Force fast charge toggle
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: LorDClockaN <lordclockan@gmail.com>
    Co-authored-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    Co-authored-by: SKULSHADY <anushekprasal@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ src/org/evolution/pixel/PixelParts/Utils.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2018-2022 crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.evolution.pixel.PixelParts;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Utils {
+
+    /**
+     * Write a string value to the specified file.
+     * @param filename      The filename
+     * @param value         The value
+     */
+    public static void writeValue(String filename, String value) {
+        if (filename == null) {
+            return;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(new File(filename));
+            fos.write(value.getBytes());
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String readLine(String filename) {
+        if (filename == null) {
+            return null;
+        }
+        BufferedReader br = null;
+        String line = null;
+        try {
+            br = new BufferedReader(new FileReader(filename), 1024);
+            line = br.readLine();
+        } catch (IOException e) {
+            return null;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
+        }
+        return line;
+    }
+
+    public static String getFileValue(String filename, String defValue) {
+        String fileValue = readLine(filename);
+        if (fileValue != null) {
+            return fileValue;
+        }
+        return defValue;
+    }
+
+    public static boolean getFileValueAsBoolean(String filename, boolean defValue) {
+        String fileValue = readLine(filename);
+        if (fileValue != null) {
+            return (fileValue.equals("0") ? false : true);
+        }
+        return defValue;
+    }
+
+    /**
+     * Check if the specified file exists.
+     * @param filename      The filename
+     * @return              Whether the file exists or not
+     */
+    public static boolean fileExists(String filename) {
+        if (filename == null) {
+            return false;
+        }
+        return new File(filename).exists();
+    }
+
+    public static boolean fileWritable(String filename) {
+        return fileExists(filename) && new File(filename).canWrite();
+    }
+}
