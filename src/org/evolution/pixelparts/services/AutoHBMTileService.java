6ae256f42c5171e7b5ea8a61d1f6d8c5ce122330
AnierinB <anierin@evolution-x.org>
2023-05-26 13:05:06 -0700

    PixelParts: Add QuickSettings tile for AutoHBM
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ src/org/evolution/pixelparts/services/AutoHBMTileService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2023 The Evolution X Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.evolution.pixelparts.services;
+
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import org.evolution.pixelparts.misc.Constants;
+import org.evolution.pixelparts.utils.AutoHBMUtils;
+
+public class AutoHBMTileService extends TileService {
+
+    private void updateTile(boolean enabled) {
+        final Tile tile = getQsTile();
+        tile.setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.updateTile();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        updateTile(sharedPrefs.getBoolean(Constants.KEY_AUTO_HBM, false));
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        final boolean enabled = !(sharedPrefs.getBoolean(Constants.KEY_AUTO_HBM, false));
+        sharedPrefs.edit().putBoolean(Constants.KEY_AUTO_HBM, enabled).commit();
+        AutoHBMUtils.enableAutoHBM(this);
+        updateTile(enabled);
+    }
+}
