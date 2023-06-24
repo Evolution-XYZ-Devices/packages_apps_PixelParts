6a9b0f4915ca960123814c72f45e56faa7205057
AnierinB <anierin@evolution-x.org>
2023-06-23 17:38:54 -0700

    PixelParts: Use UsageProgressBarPreference for current_lux_level
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/evolution/pixelparts/autohbm/AutoHBM.java
+++ src/org/evolution/pixelparts/autohbm/AutoHBM.java
@@ -20,34 +20,47 @@ import androidx.preference.PreferenceManager;
 
 import com.android.settingslib.widget.MainSwitchPreference;
 import com.android.settingslib.widget.OnMainSwitchChangeListener;
+import com.android.settingslib.widget.UsageProgressBarPreference;
 
 import org.evolution.pixelparts.misc.Constants;
+import org.evolution.pixelparts.preferences.CustomSeekBarPreference;
 import org.evolution.pixelparts.R;
 import org.evolution.pixelparts.utils.AutoHBMUtils;
 
-public class AutoHBM extends PreferenceFragment implements OnMainSwitchChangeListener, SensorEventListener {
+public class AutoHBM extends PreferenceFragment
+        implements OnMainSwitchChangeListener, SensorEventListener, Preference.OnPreferenceChangeListener {
+
     private static final String TAG = AutoHBM.class.getSimpleName();
-
-    private MainSwitchPreference mAutoHBMSwitch;
-    private SensorManager mSensorManager;
-    private Sensor mLightSensor;
-
-    private static final String[] AUTO_HBM_PREFERENCES = new String[] {
+    private static final String[] AUTO_HBM_PREFERENCES = {
             Constants.KEY_AUTO_HBM_THRESHOLD,
             Constants.KEY_AUTO_HBM_ENABLE_TIME,
             Constants.KEY_AUTO_HBM_DISABLE_TIME,
             Constants.KEY_CURRENT_LUX_LEVEL
     };
 
+    private CustomSeekBarPreference mAutoHBMThresholdPreference;
+    private MainSwitchPreference mAutoHBMSwitch;
+    private SensorManager mSensorManager;
+    private SharedPreferences mSharedPrefs;
+    private Sensor mLightSensor;
+    private UsageProgressBarPreference mCurrentLuxLevelPreference;
+    private int mCurrentLux;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.autohbm);
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
-        Context context = getContext();
 
-        mAutoHBMSwitch = (MainSwitchPreference) findPreference(Constants.KEY_AUTO_HBM);
-        mAutoHBMSwitch.setChecked(sharedPrefs.getBoolean(Constants.KEY_AUTO_HBM, false));
+        Context context = getContext();
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        mAutoHBMSwitch = findPreference(Constants.KEY_AUTO_HBM);
+        mAutoHBMSwitch.setChecked(mSharedPrefs.getBoolean(Constants.KEY_AUTO_HBM, false));
         mAutoHBMSwitch.addOnSwitchChangeListener(this);
+
+        mAutoHBMThresholdPreference = findPreference(Constants.KEY_AUTO_HBM_THRESHOLD);
+        mAutoHBMThresholdPreference.setOnPreferenceChangeListener(this);
+
+        mCurrentLuxLevelPreference = findPreference(Constants.KEY_CURRENT_LUX_LEVEL);
 
         mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
@@ -73,8 +86,7 @@ public class AutoHBM extends PreferenceFragment implements OnMainSwitchChangeLis
 
     @Override
     public void onSwitchChanged(Switch switchView, boolean isChecked) {
-        SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
-        prefChange.putBoolean(Constants.KEY_AUTO_HBM, isChecked).apply();
+        mSharedPrefs.edit().putBoolean(Constants.KEY_AUTO_HBM, isChecked).apply();
         AutoHBMUtils.enableAutoHBM(getContext());
         toggleAutoHBMPreferencesVisibility(isChecked);
 
@@ -82,6 +94,44 @@ public class AutoHBM extends PreferenceFragment implements OnMainSwitchChangeLis
             mSensorManager.registerListener(this, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
         } else {
             mSensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mAutoHBMThresholdPreference && mCurrentLuxLevelPreference != null) {
+            int threshold = (int) newValue;
+            updateCurrentLuxLevelPreference(mCurrentLux, threshold); 
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT && mCurrentLuxLevelPreference != null) {
+            float luxValue = event.values[0];
+            mCurrentLux = (int) luxValue;
+            int threshold = mSharedPrefs.getInt(Constants.KEY_AUTO_HBM_THRESHOLD, 20000);
+            updateCurrentLuxLevelPreference(mCurrentLux, threshold);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do nothing
+    }
+
+    private void updateCurrentLuxLevelPreference(int currentLux, int threshold) {
+        if (mCurrentLuxLevelPreference != null) {
+            mCurrentLuxLevelPreference.setUsageSummary(String.valueOf(currentLux));
+            mCurrentLuxLevelPreference.setTotalSummary(String.valueOf(threshold));
+
+            if (currentLux >= threshold) {
+                mCurrentLuxLevelPreference.setPercent(100, 100);
+            } else {
+                mCurrentLuxLevelPreference.setPercent(currentLux, threshold);
+            }
         }
     }
 
@@ -96,21 +146,5 @@ public class AutoHBM extends PreferenceFragment implements OnMainSwitchChangeLis
 
     public static boolean isAutoHBMEnabled(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(Constants.KEY_AUTO_HBM, false);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
-            Preference pref = findPreference(Constants.KEY_CURRENT_LUX_LEVEL);
-            if (pref != null) {
-                int luxValue = (int) event.values[0];
-                pref.setSummary(String.valueOf(luxValue));
-            }
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Do nothing
     }
 }
